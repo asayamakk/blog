084c17a95233a38a8d0cc1da2404639298f9247e
asayamakk <kodai.asayama@gmail.com>
2021-01-04 00:04:34 +0900

    enable analytics

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import VueGtag from 'vue-gtag'
+
 // Import main css
 import config from '../gridsome.config'
 import '~/assets/style/index.scss'
@@ -7,6 +9,18 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function (Vue, {router, head, isClient}) {
+  if (isClient) {
+    Vue.use(
+      VueGtag,
+      {
+        config: {id: 'G-KLNR7XHYCD'},
+        includes: [
+          {id: 'UA-145286842-1'}
+        ]
+      },
+      router,
+    )
+  }
   head.meta.push(
     {key: "og:site_name", property: "og:site_name", content: config.siteName},
   )
